9efc5a4525d01c46838134368f159dcab8188fb5
riddhijs <riddhi26.patel@gmail.com>
2025-02-16 16:59:45 -0500

    server setup

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+// Route imports
+// congigration
+dotenv.config();
+const app = express();
+app.use(express.json());
+// app.use(helmet());
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('home route');
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('server running');
+});
